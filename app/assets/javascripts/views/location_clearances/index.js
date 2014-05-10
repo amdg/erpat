9277595de80f7abcbdfec7b66c9649d151580566
MJ Alvarez <mj.alvarez@belldigitalmedia.com.au>
2014-05-10 09:40:28 +0800

    search docs

--- app/assets/javascripts/views/location_clearances/index.js
+++ app/assets/javascripts/views/location_clearances/index.js
@@ -21,40 +21,19 @@ com.glados.views.locational_clearances.index = {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    var contentString = '<div class="marker-info-win">'+
-      '<div class="marker-inner-win"><span class="info-content">'+
-      '<h1 class="marker-heading">Apply for this location</h1><p><div class="marker-edit">'+
-      '<form action="/locational_clearances" method="POST" name="SaveMarker" id="SaveMarker">'+
-      '<label for="full_name"><span>Full Name :</span><input type="text" name="full_name" placeholder="Full Name" maxlength="40" /></label>'+
-      '<label for="contact_number"><span>Contact Number :</span><input type="text" name="contact_number" placeholder="Contact Number" maxlength="40" /></label>'+
-      '<label for="street_address"><span>Street Address :</span><input type="text" name="street_address" placeholder="Street Address" maxlength="40" /></label>'+
-      '<label for="land_use"><span>Land Use :</span> <select name="land_use" class="save-type"><option value="Agricultural">Agricultural</option>' +
-      '<option value="Agricultural Nursery">Agricultural Nursery</option>'+
-      '<option value="Agri-industrial">Agri-industrial</option>' +
-      '<option value="Cemeteries">Cemeteries</option>' +
-      '<option value="Commercial">Commercial</option>' +
-      '<option value="Cemeteries">Cemeteries</option>' +
-      '<option value="Dumpsite">Dumpsite</option>' +
-      '<option value="Ecopark">Ecopark</option>' +
-      '<option value="Agro-ecotourism">Agro-ecotourism</option>' +
-      '<option value="Forest Reserves">Forest Reserves</option>' +
-      '<option value="Industrial">Industrial</option>' +
-      '<option value="Institutional">Institutional</option>' +
-      '<option value="Irrigable Land">Irrigable Land</option>' +
-      '<option value="Parks">Parks</option>' +
-      '<option value="Proposed Dumpsite">Proposed Dumpsite</option>' +
-      '<option value="Residential">Residential</option>' +
-      '<option value="River">River</option>' +
-      '<option value="Transport/Utilities">Transport/Utilities</option>' +
-      '</select></label>'+
-      '<label for="purpose"><span>Purpose :</span><textarea name="purpose" class="save-desc" placeholder="Enter Purpose" maxlength="150"></textarea></label>'+
-      '<input type="hidden" name="lat" id="loc_lat" value="0" />'+
-      '<input type="hidden" name="long" id="loc_long" value="0" />'+
-      '</form>'+
-      '</div></p><a href="#" class="btn btn-primary save-marker">Submit Application</a>';
-
+    var $contentString = $('<div class="timeline-badge">' +
+      '</div><div class="timeline-panel">      ' +
+      '<div class="timeline-heading">        ' +
+      '<h4 class="timeline-title" id="applicant-name">Mussum ipsum cacilds</h4>' +
+      '<p><small class="text-muted">' +
+      '<i class="glyphicon glyphicon-time" id="applicant-created"></i> 11 hours ago via Twitter</small></p>      ' +
+      '</div>' +
+      '<p><strong id="applicant-land-use"></strong></p>' +
+      '<div class="timeline-body">' +
+      '<p id="applicant-purpose">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>' +
+      '</div></div>');
     view.infowindow = new google.maps.InfoWindow({
-      content: contentString,
+      content: $contentString.text(),
       maxWidth: 500,
       width: 500
     });
@@ -75,10 +54,25 @@ com.glados.views.locational_clearances.index = {
 
     }
 
-    $.get('/locational_clearances/queued.json', function(data){
-      console.log(data);
-      $("#search-doc").typeahead({ source: data });
-    },'json');
+    var docs = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('id'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: '/locational_clearances/queued.json',
+      remote: '/locational_clearances/queued.json'
+    });
+    docs.initialize();
+
+    $('#remote .typeahead').typeahead(null, {
+      name: 'lc-docs',
+      displayKey: 'id',
+      source: docs.ttAdapter()
+    }).on('typeahead:selected', function (obj, datum) {
+      view.addMarker(new google.maps.LatLng(datum.lat,datum.long));
+      console.log($contentString.find('#applicant-name'));
+      $contentString.find('#applicant-name').html(datum.full_name);
+      $contentString.find('#applicant-purpose').html(datum.purpose);
+      $contentString.find('#applicant-land-use').html(datum.land_use);
+    });
 
 
   },
@@ -94,7 +88,8 @@ com.glados.views.locational_clearances.index = {
     window.setTimeout(function() {
       view.map.panTo(view.marker.getPosition());
       view.map.setZoom(16);
-    });
+      view.infowindow.open(view.map,view.marker);
+    },1000);
   },
 
   placeMarker: function(location) {
